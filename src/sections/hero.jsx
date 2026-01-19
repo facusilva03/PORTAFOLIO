b123053789c2d6b7ef4f2d52f8ad3d0f9b9dc414
facu <facusilva03@users.noreply.github.com>
2026-01-19 16:11:16 -0300

    Hero section

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,16 +1,22 @@
 import react from 'react';
 import styles from './hero.module.css';
-
+import photoProfile from '../assets/facundo-profile.png';
+import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Hero = () => {
     return (
         <section className={styles.heroContainer}>
+
             <div className={styles.content}>
                 <p className={styles.introText}>MY NAME IS </p>
-                <h1 className={styles.name}>FACUNDO<span className={styles.lastName}>SILVA</span> </h1>
-                <p className={styles.description}>Full Stack Developer based in Argentina </p>
+                <h1 className={styles.name}>FACUNDO<span className={styles.lastName}>SILVA</span>
+                </h1>
+                <p className={styles.description}>
+                    Full Stack Developer based in Argentina.
+                    Transforming ideas into digital reality.
+                </p>
 
-                <button className={styles.ctaButtom}>Contacto</button>
+                <button className={styles.ctaButton}>Contacto</button>
 
                 <div className={styles.contactInfo}>
                     <span>+54 2616152411</span>
@@ -18,8 +24,28 @@ const Hero = () => {
                 </div>
             </div>
 
-            <div className={styles.imageContainer}></div>
-                <div className={styles.circleDecorator}></div>
+            <div className={styles.imageContainer}>
+
+                <div className={styles.heroCircle}></div>
+
+                <img src={photoProfile} alt="Facundo Silva" className={styles.profileImage}/>
+
+            </div>
+
+            <div className={styles.socialBar}>
+                <a href="https://www.instagram.com/facu_silva/" target='_blank' className={styles.socialIcon}>
+                    <FaInstagram />
+                </a>
+                <a href="https://www.linkedin.com/in/facundo-silva-33a4b6342/" target='_blank' className={styles.socialIcon}>
+                    <FaLinkedin />
+                </a>
+                <a href="https://github.com/facusilva03" target='_blank' className={styles.socialIcon}>
+                    <FaGithub />
+                </a>
+
+            <div className={styles.socialLine}></div>
+            </div>
+
         </section>
     );
 }
